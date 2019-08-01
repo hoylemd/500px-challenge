52890510d31b32dc6a57be10a43e8f1da817c078
Michael D. Hoyle <hoylemd@gmail.com>
2019-08-01 15:30:04 -0400

    refactor python app to be bff (back-end-for-front-end)

--- src/app.py
+++ src/app.py
@@ -1,7 +1,6 @@
 import logging
-from flask import Flask, render_template, request
+from flask import Flask, request, jsonify
 
-from utils import make_pager_url
 from five_hundred_px import FiveHundredPX
 
 app = Flask(__name__)
@@ -11,7 +10,7 @@ logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.DEBUG)
 
 
-@app.route('/')
+@app.route('/bff/')
 def index():
     rpp = int(request.args.get('rpp', 8))
     page = request.args.get('page', None)
@@ -20,38 +19,17 @@ def index():
     response = api.get_feed(rpp=rpp, page=page)
 
     raw = response.json()
-    pages = raw['total_pages']
-    photos = raw['photos']
 
-    page = int(page or 1)
-
-    if page > pages:
-        page = pages
-
-    context = {
-        'photos': photos,
-        'n': len(photos),
-        'rpp': rpp,
-        'page': page,
-        'pages': pages,
-        'prev_link': make_pager_url(rpp=rpp, page=page - 1, total=pages),
-        'next_link': make_pager_url(rpp=rpp, page=page + 1, total=pages)
-    }
-
-    return render_template('index.html', **context)
+    return jsonify(raw)
 
 
-@app.route('/<int:photo_id>')
+@app.route('/bff/<int:photo_id>')
 def detail(photo_id):
 
     api = FiveHundredPX()
     raw = api.get_detail(photo_id).json()
 
-    context = {
-        'photo': raw['photo']
-    }
-
-    return render_template('detail.html', **context)
+    return jsonify(raw)
 
 
 if __name__ == '__main__':
