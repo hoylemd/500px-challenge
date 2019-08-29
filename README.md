5a92c2c87bc6bf6e6d64214a39d1755d9ec077e2
Michael D. Hoyle <hoylemd@gmail.com>
2019-08-29 11:53:26 -0400

    fix some formatting for easier reading

--- README.md
+++ README.md
@@ -32,38 +32,37 @@ case. Untimately, I decided not to worry about it as the early pages work
 correctly and I didn't want to spend too much time on such a rare use case.
 
 I also ran into some trouble using the `photo_size` queryparam in the api. I
-would request a specific size code (3, 30, etc), but I'd only ever get
-size-code 22 urls in the `images` field of the response. I tried putting the
-size codes in arrays on Pawan's suggestion, but it didn't seem to have any
-effect.  To rectify this, I simply used css to re-scale the images on the main
-feed page to fit 8 of them on screen. In a real project, this would be at best
-a temporary stopgap. As the app is requesting the full-sized images every time
-it loads a feed page, page load time will be impacted, as well as using up a
-lot of extra bandwidth. Using the pre-set image sizes would also be more
-helpful on the layout of the page. I could use cropped square images as
-thumbnails, meaning that all thumbnails are the same size, so each 'card' could
-have identical dimensions. Instead, I used `max-width` and `max-height` styles
-to ensure the images fit on a card *roughly* 300px x 300px, which allowed 8 to
-fit nicely on my laptop screen.
+would request a specific size code (3, 30, etc), but I'd only ever get size-code
+22 urls in the `images` field of the response. I tried putting the size codes in
+arrays on Pawan's suggestion, but it didn't seem to have any effect.  To rectify
+this, I simply used css to re-scale the images on the main feed page to fit 8 of
+them on screen. In a real project, this would be at best a temporary stopgap. As
+the app is requesting the full-sized images every time it loads a feed page,
+page load time will be impacted, as well as using up a lot of extra bandwidth.
+Using the pre-set image sizes would also be more helpful on the layout of the
+page. I could use cropped square images as thumbnails, meaning that all
+thumbnails are the same size, so each 'card' could have identical dimensions.
+Instead, I used `max-width` and `max-height` styles to ensure the images fit on
+a card *roughly* 300px x 300px, which allowed 8 to fit nicely on my laptop
+screen.
 
 This implementation may also run a bit on the slower side, as the
 request-response exchange won't complete until the entire request/response sent
-to the 500px api completes. In practice, the 500px api is responsive enough
-that the additional response overhead is barely noticeable, and the page
-doesn't have anything useful to show to the user until that request is
-completed anyway, so making the API requests with ajax from a javascript
-application would only shift that 'delay' to after the single-page app has
-loaded.
+to the 500px api completes. In practice, the 500px api is responsive enough that
+the additional response overhead is barely noticeable, and the page doesn't have
+anything useful to show to the user until that request is completed anyway, so
+making the API requests with ajax from a javascript application would only shift
+that 'delay' to after the single-page app has loaded.
 
 ## Design summary
 
 I chose to use Flask as my web framework as python is my strongest language by
-far, and the requirements didn't seem to require any more advanced
-functionality than simple request handling. I was able to make the app (almost)
-entirely stateless however, with no need for user accounts or any persisted
-data, it can be (almost) entirely functional in nature. The only 'state' that
-it needs is the API key and url to the api endpoint (which has a sensible
-default... the actual api url).
+far, and the requirements didn't seem to require any more advanced functionality
+than simple request handling. I was able to make the app (almost) entirely
+stateless however, with no need for user accounts or any persisted data, it can
+be (almost) entirely functional in nature. The only 'state' that it needs is the
+API key and url to the api endpoint (which has a sensible default... the actual
+api url).
 
 ## React addendum
 
@@ -97,5 +96,6 @@ view, followed by arbitrary paging and tests.
 
 Beyond the requirements stated, I'd make the following changes:
 
-- productionize the servers. (e.g. gunicorn for the bff, compile the js to a minified bundle + serve by productionized nginx.)
-- add a testing container to do subcutaneous testing
+- productionize the servers. (e.g. gunicorn for the bff, compile the js to a
+- minified bundle + serve by productionized nginx.) add a testing container to
+- do subcutaneous testing
--- spa_src/src/index.js
+++ spa_src/src/index.js
@@ -26,48 +26,48 @@ PhotoCard.propTypes = {
 };
 
 class Pager extends React.Component {
-    render () {
-        let prevButton;
-        let nextButton;
+  render () {
+    let prevButton;
+    let nextButton;
 
-        if ((this.props.page - 1) > 0) {
-          prevButton = (
-            <button className='pager-prev' onClick={this.props.prevPage}>
-              &lt;-
-            </button>
-          );
-        } else {
-          prevButton = (
-            <button className='pager-prev disabled' disabled='true'>
-              &lt;-
-            </button>
-          );
-        }
-
-        if ((this.props.page) < this.props.pages) {
-          nextButton = (
-            <button className='pager-next' onClick={this.props.nextPage}>
-              -&gt;
-            </button>
-          );
-        } else {
-          nextButton = (
-            <button className='pager-next disabled' disabled='true'>
-              -&gt;
-            </button>
-          );
-        }
-
-        return (
-            <div className="pager">
-                {prevButton}
-                <span className='page_number'>
-                  {this.props.page} / {this.props.pages}
-                </span>
-                {nextButton}
-            </div>
-        );
+    if ((this.props.page - 1) > 0) {
+      prevButton = (
+        <button className='pager-prev' onClick={this.props.prevPage}>
+          &lt;-
+        </button>
+      );
+    } else {
+      prevButton = (
+        <button className='pager-prev disabled' disabled='true'>
+          &lt;-
+        </button>
+      );
     }
+
+    if ((this.props.page) < this.props.pages) {
+      nextButton = (
+        <button className='pager-next' onClick={this.props.nextPage}>
+          -&gt;
+        </button>
+      );
+    } else {
+      nextButton = (
+        <button className='pager-next disabled' disabled='true'>
+          -&gt;
+        </button>
+      );
+    }
+
+    return (
+      <div className="pager">
+        {prevButton}
+        <span className='page_number'>
+          {this.props.page} / {this.props.pages}
+        </span>
+        {nextButton}
+      </div>
+    );
+  }
 }
 Pager.propTypes = {
   page: PropTypes.number,
@@ -77,109 +77,110 @@ Pager.propTypes = {
 };
 
 class PhotoPage extends React.Component {
-    createCard(photo) {
-        return (
-            <PhotoCard
-                id={photo.id}
-                name={photo.name}
-                src={photo.images[0].https_url}
-                author={photo.user.username}
-            />
-        );
+  createCard(photo) {
+    return (
+      <PhotoCard
+        id={photo.id}
+        name={photo.name}
+        src={photo.images[0].https_url}
+        author={photo.user.username}
+      />
+    );
+  }
+
+  createCards() {
+    let cards = [];
+    for (let i=0; i < this.props.photos.length; i +=1) {
+      cards.push(this.createCard(this.props.photos[i]));
     }
 
-    createCards() {
-        let cards = [];
-        for (let i=0; i < this.props.photos.length; i +=1) {
-            cards.push(this.createCard(this.props.photos[i]));
-        }
+    return cards;
+  }
 
-        return cards;
-    }
-
-    render() {
-        return (
-            <div className="photo_page">
-              <h3>
-                The current most popular {this.props.photos.length} photos on 500px
-              </h3>
-              {this.createCards()}
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="photo_page">
+        <h3>
+          The current most popular {this.props.photos.length} photos on 500px
+        </h3>
+        {this.createCards()}
+      </div>
+    );
+  }
 }
 PhotoPage.propTypes = {
   photos: PropTypes.array
 };
 
 class Showcase extends React.Component {
-    constructor(props) {
-        super(props);
+  constructor(props) {
+    super(props);
 
-        this.state = {
-            photos: null,
-            rpp: 8,
-            page: 1,
-            pages: 1,
-        };
+    this.state = {
+      photos: null,
+      rpp: 8,
+      page: 1,
+      pages: 1,
+    };
+  }
+
+  getPhotos(page) {
+    let url = '/bff/?rpp=' + this.state.rpp + '&page=' + page;
+
+    window.fetch(url).then((response) => {
+      return response.json();
+    }).then((blob) => {
+      this.setState({
+        photos: blob.photos,
+        page: blob.current_page,
+        pages: blob.total_pages
+      });
+    });
+  }
+
+  nextPage() {
+    let target = this.state.page + 1;
+    if (target > this.state.pages) {
+      return;
     }
 
-    getPhotos(page) {
-        let url = '/bff/?rpp=' + this.state.rpp + '&page=' + page;
+    this.getPhotos(target);
+  }
 
-        window.fetch(url).then((response) => {
-            return response.json();
-        }).then((blob) => {
-            this.setState({
-                photos: blob.photos,
-                page: blob.current_page,
-                pages: blob.total_pages
-            });
-        });
+  prevPage() {
+    let target = this.state.page - 1;
+    if (target < 1) {
+      return;
     }
 
-    nextPage() {
-        let target = this.state.page + 1;
-        if (target > this.state.pages) {
-            return;
-        }
+    this.getPhotos(target);
+  }
 
-        this.getPhotos(target);
+  render() {
+    if (this.state.photos === null) {
+      this.getPhotos(1);
+      return 'loading...';
+    } else {
+      return (
+        <div className='showcase'>
+          <PhotoPage
+            photos={this.state.photos}
+            page={this.state.page}
+            rpp={this.state.rpp}
+          />
+          <Pager
+            page={this.state.page}
+            pages={this.state.pages}
+            nextPage={() => this.nextPage()}
+            prevPage={() => this.prevPage()}
+          />
+        </div>
+      );
     }
-
-    prevPage() {
-        let target = this.state.page - 1;
-        if (target < 1) {
-            return;
-        }
-
-        this.getPhotos(target);
-    }
-
-    render() {
-        if (this.state.photos === null) {
-            this.getPhotos(1);
-            return 'loading...';
-        } else {
-            return (
-                <div className='showcase'>
-                    <PhotoPage
-                        photos={this.state.photos}
-                        page={this.state.page}
-                        rpp={this.state.rpp}
-                    />
-                    <Pager
-                        page={this.state.page}
-                        pages={this.state.pages}
-                        nextPage={() => this.nextPage()}
-                        prevPage={() => this.prevPage()}
-                    />
-                </div>
-            );
-        }
-    }
+  }
 }
+
 ReactDOM.render(
-    <Showcase />,
-    document.getElementById('root')
+  <Showcase />,
+  document.getElementById('root')
 );
